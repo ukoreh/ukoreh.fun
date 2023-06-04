07278b75a14722131f055905945c0022e0f37164
freitzzz <1160907@isep.ipp.pt>
2023-06-04 23:29:20 +0100

    Updates

---
+++ _app/immutable/chunks/index.51114b2a.js
@@ -0,0 +1,1 @@
+const e={title:"🧙‍♂️ Ukoreh 🧙‍♂️",description:"Deploy any Flutter project as a web app at the distance of a click!",deploy:"Deploy",workflowDividerLabel:"Workflow Status",startingWorkflow:"Starting deploy workflow...",feelingLucky:"I'm feeling lucky 🤞"};export{e as default};
